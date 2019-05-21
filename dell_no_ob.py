06f9bc8be5cfb4787dc327f5d92cdb8db8588b00
lovepan1 <39023964+lovepan1@users.noreply.github.com>
2019-05-21 17:01:19 +0800

    Add files via upload

---
+++ dell_no_ob.py
@@ -0,0 +1,73 @@
+import xml.etree.ElementTree as ET
+from os import getcwd
+import os
+
+sets = [('dianli', 'test')]
+
+classes = ["DiaoChe", "TaDiao", "TuiTuJi", "BengChe", "WaJueJi", "ChanChe", "SuLiaoBu", "FengZheng", "Niao", "NiaoWo",
+           "ShanHuo", "YanWu", "JianGeBang", "JueYuanZi", "FangZhenChui"]
+img_dir = "/home/pcl/data/VOC2007/JPEGImagesTest"
+anno_dir = "/home/pcl/data/VOC2007/AnnotationsTest"
+
+
+def convert_annotation(year, image_id, list_file):
+    # in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    # anno_id =
+    in_file = open(anno_dir + '/%s.xml' % image_id.split('.jpg')[0])
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+
+    for obj in root.iter('object'):
+        try:
+            difficult = obj.find('difficult').text
+        except:
+            difficult = 0
+        cls = obj.find('name').text
+        if cls not in classes :
+            # print(cls)
+            # print("image_id:{} has :{} object".format(image_id, cls))
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text),
+             int(xmlbox.find('ymax').text))
+        list_file.write(" " + str(cls_id) + " " + " ".join([str(a) for a in b]))
+
+def is_no_obj(image_id):
+    try:
+        in_file = open(anno_dir + '/%s.xml' % image_id.split('.jpg')[0])
+    except:
+        print("lost file is:{}".format(anno_dir + '/%s.xml' % image_id.split('.jpg')[0]))
+        return True
+    obj_num = 0
+    for line in in_file:
+        if "object" in line:
+            obj_num  = obj_num + 1
+    obj_num = obj_num / 2
+    if obj_num != 0:
+        return False
+    else:
+        print(image_id)
+        print('0 != 0.0')
+        return True
+
+wd = getcwd()
+
+for year, image_set in sets:
+    # image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+    image_ids = os.listdir(img_dir)
+    print("[INFO] the current img nums is %d" % len(image_ids))
+    num = 0
+    list_file = open('%s_%s.txt' % (year, image_set), 'w')
+    for image_id in image_ids:
+        # print(image_id)
+        # num = num + 1
+        if (num % 1000 == 0):
+            print("current write img_num is %d" % num)
+        if not is_no_obj(image_id):
+            list_file.write(str(num) + ' ' + img_dir + '/%s' % (image_id))
+            convert_annotation(year, image_id, list_file)
+            num = num + 1
+            list_file.write('\n')
+    list_file.close()
+
